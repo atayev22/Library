b44282e44523dc1b1a41cad27a9a44a97b7c5487
kamran atayev <atayev.k@bk.ru>
2023-02-20 15:57:19 +0400

    add-AuthorMethod-Delete

--- LibraryAPI.Business/Services/Concrete/AuthorService.cs
+++ LibraryAPI.Business/Services/Concrete/AuthorService.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using AutoMapper;
+using Core.Utilities.Results;
+using LibraryAPI.Business.Services.Abstract;
+using LibraryAPI.Core.Entities.Dtos.AuthorDtos;
+using LibraryAPI.Core.Entities.Dtos.BookDtos;
+using LibraryAPI.DataAccess.Entities.Models;
+using LibraryAPI.DataAccess.Infrastructure.Repositories.Concrete;
+using LibraryAPI.DataAccess.Repositories.Abstract;
+using LibraryAPI.DataAccess.Repositories.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +15,74 @@ using System.Threading.Tasks;
 
 namespace LibraryAPI.Business.Services.Concrete
 {
-    public class AuthorService
+    public class AuthorService : IAutorService
     {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorService(IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+        public Result GetAuthorsBrose()
+        {
+            var result = new Result();
+
+            var data = _mapper.Map<IEnumerable<AuthorBrowseDto>>(_authorRepository.GetAll().ToList());
+            result.Data = data;
+
+            return result;
+        }
+
+        public Result GetAuthorById(int id)
+        {
+            var result = new Result();
+
+            var author = _authorRepository.Get(id);
+            result.Data = author;
+            if (result.Data == null)
+            {
+                return result;
+            }
+            return result;
+        }
+
+        public Result GetAuthorByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultInfo AddOrUpdateAuthor(Author author)
+        {
+            var result = new Result();
+            if(author.Id is 0)
+            {
+               _authorRepository.Add(author);
+            }
+            else
+            {
+                _authorRepository.Update(author);
+            }
+
+            if (author is null)
+            {
+                return ResultInfo.NotImplemented;
+            }
+
+            return ResultInfo.Success;
+
+        }
+
+        public ResultInfo DeleteAuthor(int id)
+        {
+            var response = _authorRepository.Delete(id);
+            if (response is false)
+            {
+                return ResultInfo.NotFound;
+            }
+            return ResultInfo.Deleted;
+        }
+       
     }
 }
