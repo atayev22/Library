bc3ee6bc3c415812862d189deace8f8b5ff3e960
kamran atayev <atayev.k@bk.ru>
2023-02-24 17:20:41 +0400

    UserService

--- LibraryAPI.DataAccess/Repositories/Abstract/IUserRepository.cs
+++ LibraryAPI.DataAccess/Repositories/Abstract/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryAPI.DataAccess.Infrastructure.Repositories.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LibraryAPI.DataAccess.Repositories.Abstract
 {
-    internal interface IUserRepository
+    public interface IUserRepository : IEntityBaseRepository
     {
     }
 }
