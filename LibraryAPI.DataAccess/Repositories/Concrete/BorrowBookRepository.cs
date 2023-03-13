ebf05533d5e5fe311545a6e128d4a84dd82f4d75
kamran atayev <atayev.k@bk.ru>
2023-03-13 11:28:46 +0400

    api

--- LibraryAPI.DataAccess/Repositories/Concrete/BorrowBookRepository.cs
+++ LibraryAPI.DataAccess/Repositories/Concrete/BorrowBookRepository.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using LibraryAPI.DataAccess.Context;
+using LibraryAPI.DataAccess.Entities.Models;
+using LibraryAPI.DataAccess.Infrastructure.Repositories.Concrete;
+using LibraryAPI.DataAccess.Repositories.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,15 @@ using System.Threading.Tasks;
 
 namespace LibraryAPI.DataAccess.Repositories.Concrete
 {
-    internal class BorrowBookRepository
+    public class BorrowBookRepository : EntityBaseRepository<BorrowedBook>, IBorrowBookRepository
     {
+        public BorrowBookRepository(AppDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+
+        public IQueryable<Book> GetBorrowBooksBrowse()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
