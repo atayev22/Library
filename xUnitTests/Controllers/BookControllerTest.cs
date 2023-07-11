ec22d3d2f8f75f29b9b36b72fbcc35ee0802d646
kamran atayev <atayev.k@bk.ru>
2023-07-11 17:24:12 +0400

    kamran

---
+++ xUnitTests/Controllers/BookControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTests.Controllers
+{
+    public class BookControllerTest
+    {
+    }
+}
